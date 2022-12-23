06974011d32b3b4a64c272e5568ac42a8fa59116
farokojil <farok.ojil@gmail.com>
2022-12-23 18:20:40 +0100

    refactor parsing

--- app/core/ksqlDB.ts
+++ app/core/ksqlDB.ts
@@ -33,27 +33,30 @@ export const getTickerInfo = async (ticker: string) => {
   });
 
   const responseText = await response.text();
-  const responseArray = responseText.split("\n");
-  const headerRow = responseArray.shift();
-  const headerObj: MetaData = JSON.parse(headerRow as string);
-  const headers = headerObj.columnNames;
-  console.log(headers);
-  const infoItems = responseArray.map((row) => {
-    if (row == "") {
-      return;
-    }
 
-    console.log(row.replace(/\s/g, ""));
-    const rowJSON = JSON.parse(row.replace(/\s/g, ""));
-    const item: InfoItem = { symbol: rowJSON[0], eventTime: rowJSON[1] };
-    return item;
-  });
-
-  // const info = parseResponse(responseText);
+  const infoItems = parseResponse(responseText);
 
   return infoItems;
 };
 
 function getTickerQuery(ticker: string): string {
   return `SELECT * FROM INFO_TEST3 WHERE SYMBOL='${ticker}';`;
+}
+
+function parseResponse(responseText: string): InfoItem[] {
+  const responseArray = responseText.split("\n");
+  const headerRow = responseArray.shift();
+  const headerObj: MetaData = JSON.parse(headerRow as string);
+  const headers = headerObj.columnNames;
+  console.log(headers);
+
+  return responseArray.map((row) => {
+    if (row == "") {
+      return {} as InfoItem;
+    }
+
+    const rowJSON = JSON.parse(row);
+    const item: InfoItem = { symbol: rowJSON[0], eventTime: rowJSON[1] };
+    return item;
+  });
 }
